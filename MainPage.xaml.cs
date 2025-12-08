9aa8fb9ab9b171680bfa63d44a59fd01abab333e
Ilya Gladkikh <moskariko@mail.ru>
2025-12-08 10:19:54 +0200

    Doggo app
    Doggo app

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var response = await _httpClient
+                .GetFromJsonAsync<DogResponse>("https://dog.ceo/api/breeds/image/random");
+
+            DogImage.Source = response.Message;
+        }
+    }
+
+    public class DogResponse
+    {
+        public string Message { get; set; }
+        public string Status { get; set; }
+    }
+}
